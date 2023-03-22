0d7877c2f4212873a2ae67664a15b27243fa8d9a
JJOOSSHHHH <joshuadrummer14@gmail.com>
2023-03-23 00:33:34 +0800

    Updated Create Account

--- app/src/main/java/com/hygeia/CreateAccountActivity.kt
+++ app/src/main/java/com/hygeia/CreateAccountActivity.kt
@@ -1,29 +1,50 @@
 package com.hygeia
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import android.widget.EditText
-import android.widget.ToggleButton
+import com.hygeia.databinding.ActivityCreateAccountBinding
+import java.util.*
 
 class CreateAccountActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityCreateAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        bind = ActivityCreateAccountBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
-        val txtNewPassword = findViewById<EditText>(R.id.txtNewPassword)
-        val tglShowPassword = findViewById<ToggleButton>(R.id.tglShowPassword)
-
-        tglShowPassword.setOnCheckedChangeListener { _, isChecked ->
+        bind.tglShowPassword.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                txtNewPassword.transformationMethod = null
-                tglShowPassword.setBackgroundResource(R.drawable.ic_visibility_off)
-                txtNewPassword.setSelection(txtNewPassword.text.length)
+                bind.txtNewPassword.transformationMethod = null
+                bind.tglShowPassword.setBackgroundResource(R.drawable.ic_visibility_off)
+                bind.txtNewPassword.setSelection(bind.txtNewPassword.text.length)
             } else {
-                txtNewPassword.transformationMethod = PasswordTransformationMethod()
-                tglShowPassword.setBackgroundResource(R.drawable.ic_visibility)
-                txtNewPassword.setSelection(txtNewPassword.text.length)
+                bind.txtNewPassword.transformationMethod = PasswordTransformationMethod()
+                bind.tglShowPassword.setBackgroundResource(R.drawable.ic_visibility)
+                bind.txtNewPassword.setSelection(bind.txtNewPassword.text.length)
             }
         }
+
+        bind.btnCalendar.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val calMonth = calendar.get(Calendar.MONTH)
+        val calDay = calendar.get(Calendar.DAY_OF_MONTH)
+        val calYear = calendar.get(Calendar.YEAR)
+        val dlgDatePicker = DatePickerDialog(
+            this,
+            { _, year, monthOfYear, dayOfMonth ->
+                val date = (dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year)
+                bind.txtNewBirthDate.setText(date)
+            },
+            calYear, calMonth, calDay
+        )
+        dlgDatePicker.show()
     }
 }
