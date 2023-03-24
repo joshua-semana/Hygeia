9b93287870cdf66c0aedb6b4445626c87ccdb02a
danicabullo <dandandalandan1500@gmail.com>
2023-03-24 23:12:11 +0800

    Added Firebase in Login and Create Account

---
+++ app/src/main/java/com/hygeia/User.kt
@@ -0,0 +1,11 @@
+package com.hygeia
+
+data class User(val id : String,
+                val gender : String,
+                val firstname : String,
+                val lastname : String,
+                val birthdate : String,
+                val email : String,
+                val password : String,
+                val phone : String,
+                val role : String)
