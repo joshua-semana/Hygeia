53d4d9923c52b35ffc098c74bd16a665e880ebd3
JJOOSSHHHH <joshuadrummer14@gmail.com>
2023-03-26 02:55:34 +0800

    'CreateAccountComplete'

--- app/src/main/java/com/hygeia/LoginActivity.kt
+++ app/src/main/java/com/hygeia/LoginActivity.kt
@@ -3,51 +3,82 @@ package com.hygeia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.getSystemService
+import androidx.core.widget.addTextChangedListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.hygeia.databinding.ActivityLoginBinding
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var bind: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
+    private lateinit var bind : ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
         auth = Firebase.auth
 
-        bind.btnGoToCreateAccount.setOnClickListener {
-            startActivity(Intent(this, CreateAccountActivity::class.java))
-//            setContentView(R.layout.activity_create_account)
-        }
         bind.btnLogin.setOnClickListener {
-            if (TextUtils.isEmpty(bind.txtUsername.text) && TextUtils.isEmpty(bind.txtPassword.text)){
-                Toast.makeText(this, "Please input the required details", Toast.LENGTH_SHORT).show()
-            }
-            auth.signInWithEmailAndPassword(bind.txtUsername.text.toString(),bind.txtPassword.text.toString())
+            //TODO: "Validate email and password, if email doesn't exist in our system, or format
+            // is incorrect, set lblLoginErrorMsg1.visibility to View.visible or View.gone"
+            auth.signInWithEmailAndPassword(bind.txtEmail.text.toString(), bind.txtPassword.text.toString())
                 .addOnCompleteListener(this){ task ->
-                    if (task.isSuccessful){
-                        Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "LoginActivity Successfully", Toast.LENGTH_SHORT).show()
                         startActivity(Intent(this, StandardMainActivity::class.java))
-                    }else{
+                    } else {
+                        bind.lblLoginErrorMsg1.visibility = View.VISIBLE
+                        bind.lblLoginErrorMsg2.visibility = View.VISIBLE
                         Toast.makeText(this, "Please input correct email or password", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
 
-
-        bind.tglShowPassword.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                bind.txtPassword.transformationMethod = null
-                bind.txtPassword.setSelection(bind.txtPassword.text.length)
-            } else {
-                bind.txtPassword.transformationMethod = PasswordTransformationMethod()
-                bind.txtPassword.setSelection(bind.txtPassword.text.length)
+        with(bind) {
+            //ELEMENT BEHAVIOR
+            layoutLogin.setOnClickListener {
+                getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+                layoutLogin.requestFocus()
+                currentFocus?.clearFocus()
             }
+
+            tglShowPassword.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    txtPassword.transformationMethod = null
+                    txtPassword.setSelection(txtPassword.text.length)
+                } else {
+                    txtPassword.transformationMethod = PasswordTransformationMethod()
+                    txtPassword.setSelection(txtPassword.text.length)
+                }
+            }
+
+            //VALIDATION
+            val textWatcher = object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+                override fun afterTextChanged(s: Editable?) {
+                    btnLogin.isEnabled = txtEmail.text.isNotEmpty() and
+                            txtPassword.text.isNotEmpty()
+                }
+            }
+
+            txtEmail.addTextChangedListener(textWatcher)
+            txtPassword.addTextChangedListener(textWatcher)
+        }
+        //NAVIGATION
+        bind.btnGoToCreateAccountPart1.setOnClickListener {
+            startActivity(Intent(this, CreateAccountPart1::class.java))
         }
     }
 }
