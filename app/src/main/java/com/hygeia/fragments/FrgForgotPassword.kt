4170063f74ee1f97e45f6e9530023f1ccd18c648
JJOOSSHHHH <joshuadrummer14@gmail.com>
2023-04-17 18:13:23 +0800

    FixedOTP

--- app/src/main/java/com/hygeia/fragments/FrgForgotPassword.kt
+++ app/src/main/java/com/hygeia/fragments/FrgForgotPassword.kt
@@ -1,6 +1,7 @@
 package com.hygeia.fragments
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
@@ -27,6 +29,10 @@ import java.util.concurrent.TimeUnit
 class FrgForgotPassword : Fragment() {
     private lateinit var bind : FrgForgotPasswordBinding
     private lateinit var auth : FirebaseAuth
+
+    private var phoneNumber: String = "+639123456789"
+    private var otp: String = "123456"
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         bind = FrgForgotPasswordBinding.inflate(inflater, container, false)
         auth = Firebase.auth
@@ -84,114 +90,60 @@ class FrgForgotPassword : Fragment() {
                         lblForgotPasswordErrorMsg1.visibility = View.VISIBLE
                         lblForgotPasswordErrorMsg1.text = getString(R.string.validate_email_exists)
                     } else {
-//                        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-//                            .addOnCompleteListener{emailCheck ->
-//                                if (emailCheck.isSuccessful){
-//                                    requireActivity().msg("Check your email")
-//                                } else {
-//                                    requireActivity().msg("Message Failed")
-//                                }
-//                            }
-                        getPhoneNumber()
+//                        getPhoneNumber(email).addOnSuccessListener { result ->
+//                            phoneNumber = result.toString()
+//                            loading.dismiss()
+//                            sendOTP(phoneNumber)
+//                        }
+                        loading.dismiss()
+                        sendOTP(phoneNumber)
                     }
                     loading.dismiss()
                 }
             }
         }
     }
-    private fun sendOTP() {
-        //TODO : SEND OTP FUNCTION, ADD 30 SECONDS GLOBAL OTP TIME COUNTER
-    }
-    private data class UserData(val phoneNumber: String)
-    private fun getPhoneNumber(): UserData {
-        //TODO : GET THE LAST 4 DIGITS OF PHONE NUMBER
-        var phoneNumber = UserData("")
-        with(bind) {
-            FirebaseFirestore.getInstance().collection("User")
-                .whereEqualTo("email", txtForgotEmail.text.toString())
-                .get()
-                .addOnSuccessListener { getPhoneNumberList ->
-                    phoneNumber = UserData(getPhoneNumberList.documents[0].getString("phoneNumber").toString())
-                    val options = PhoneAuthOptions.newBuilder(auth)
-                        .setPhoneNumber(phoneNumber.phoneNumber)   // Phone number to verify
-                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                        .setActivity(requireActivity())           // Activity (for callback binding)
-                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
-                        .build()
-                    PhoneAuthProvider.verifyPhoneNumber(options)
-                }
-                .addOnFailureListener {
-                    // handle Firestore query failure
-                    requireActivity().msg("Error getting phone number")
-                }
+
+    private fun getPhoneNumber(email: String): Task<String> {
+        val db = FirebaseFirestore.getInstance()
+        val query = db.collection("User").whereEqualTo("email", email)
+
+        return query.get().continueWith { task ->
+            val phoneNumber = task.result?.documents?.get(0)?.getString("phoneNumber").toString()
+            phoneNumber
         }
-        return phoneNumber
     }
-    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(requireActivity()) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    requireActivity().msg("Success")
-                } else {
-                    // Sign in failed, display a message and update the UI
-                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                        // The verification code entered was invalid
-                        requireActivity().msg("Invalid")
+
+    private fun sendOTP(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) { }
+                override fun onVerificationFailed(ex: FirebaseException) {
+                    if (ex is FirebaseAuthInvalidCredentialsException) {
+                        requireActivity().msg("Invalid request")
+                    } else if (ex is FirebaseTooManyRequestsException) {
+                        requireActivity().msg("Too many request")
                     }
-                    // Update UI
                 }
-            }
+                override fun onCodeSent(
+                    verificationId: String,
+                    resendToken: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    sendArguments(phoneNumber, otp, resendToken)
+                }
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
-    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        val userData = getPhoneNumber()
-        val phoneNumber = userData.phoneNumber
-        val censorPhoneNumber = "*****${phoneNumber.substring(7)}"
-        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-            // This callback will be invoked in two situations:
-            // 1 - Instant verification. In some cases the phone number can be instantly
-            //     verified without needing to send or enter a verification code.
-            // 2 - Auto-retrieval. On some devices Google Play services can automatically
-            //     detect the incoming verification SMS and perform verification without
-            //     user action.
-            signInWithPhoneAuthCredential(credential)
-        }
-        override fun onVerificationFailed(e: FirebaseException) {
-            // This callback is invoked in an invalid request for verification is made,
-            // for instance if the the phone number format is not valid.
-            if (e is FirebaseAuthInvalidCredentialsException) {
-                // Invalid request
-                requireActivity().msg("Invalid request")
-            } else if (e is FirebaseTooManyRequestsException) {
-                // The SMS quota for the project has been exceeded
-                requireActivity().msg("Too many request")
-            }
-            // Show a message and update the UI
-        }
-        override fun onCodeSent(
-            verificationId: String,
-            token: PhoneAuthProvider.ForceResendingToken
-        ) {
-            // The SMS verification code has been sent to the provided phone number, we
-            // now need to ask the user to enter the code and then construct a credential
-            // by combining the code with a verification ID.
-            // Save verification ID and resending token so we can use them later
-            val bundle = Bundle().apply {
-                putString("phoneNumber", censorPhoneNumber)
-                putString("OTP", verificationId)
-                putParcelable("resendToken", token)
-            }
-            val fragment = FrgOTP().apply { arguments = bundle }
-            parentFragmentManager.beginTransaction()
-                .setCustomAnimations(androidx.appcompat.R.anim.abc_fade_in, androidx.appcompat.R.anim.abc_fade_out)
-                .replace(R.id.containerForgotPassword, fragment)
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-    private fun sendArguments(phoneNumber : String) {
+
+    private fun sendArguments(phoneNumber : String, otp : String, token : Parcelable) {
         val bundle = Bundle().apply {
             putString("phoneNumber", phoneNumber)
+            putString("otp", otp)
+            putParcelable("token", token)
         }
         val fragment = FrgOTP().apply { arguments = bundle }
         parentFragmentManager.beginTransaction()
--- app/src/main/java/com/hygeia/fragments/FrgOTP.kt
+++ app/src/main/java/com/hygeia/fragments/FrgOTP.kt
@@ -10,8 +10,9 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.google.firebase.FirebaseException
-import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hygeia.Utilities.msg
 import com.hygeia.databinding.FrgOtpBinding
 import java.util.concurrent.TimeUnit
@@ -19,13 +20,14 @@ import java.util.concurrent.TimeUnit
 class FrgOTP : Fragment() {
     private lateinit var bind : FrgOtpBinding
     private lateinit var auth : FirebaseAuth
-    private lateinit var resendToken : PhoneAuthProvider.ForceResendingToken
-    private lateinit var otpArgument : String
+    private lateinit var argToken : PhoneAuthProvider.ForceResendingToken
+    private lateinit var argOtp : String
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         bind = FrgOtpBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
 
         with(bind) {
-            val title = "${titleOTP.text} ${arguments?.getString("phoneNumber")}"
+            val title = "${titleOTP.text} ${arguments?.getString("phoneNumber")?.substring(7)}"
             //POPULATE
             titleOTP.text = title
 
@@ -57,12 +59,22 @@ class FrgOTP : Fragment() {
     }
     private fun resendOTP() {
         val phoneNumber = arguments?.getString("phoneNumber").toString()
-        val resendToken : PhoneAuthProvider.ForceResendingToken?  = arguments?.getParcelable("resendToken")
+        val resendToken : PhoneAuthProvider.ForceResendingToken? = arguments?.getParcelable("resendToken")
         val options = PhoneAuthOptions.newBuilder(auth)
-            .setPhoneNumber(phoneNumber)       // Phone number to verify
-            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-            .setActivity(requireActivity())                 // Activity (for callback binding)
-            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) { }
+                override fun onVerificationFailed(ex: FirebaseException) { }
+                override fun onCodeSent(
+                    verificationId: String,
+                    resendToken: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    argOtp = verificationId
+                    argToken = resendToken
+                }
+            })
             .apply {
                 if (resendToken != null) {
                     setForceResendingToken(resendToken)
@@ -71,69 +83,26 @@ class FrgOTP : Fragment() {
             .build()
         PhoneAuthProvider.verifyPhoneNumber(options)
     }
-    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-            // This callback will be invoked in two situations:
-            // 1 - Instant verification. In some cases the phone number can be instantly
-            //     verified without needing to send or enter a verification code.
-            // 2 - Auto-retrieval. On some devices Google Play services can automatically
-            //     detect the incoming verification SMS and perform verification without
-            //     user action.
-            signInWithPhoneAuthCredential(credential)
-        }
 
-        override fun onVerificationFailed(e: FirebaseException) {
-            // This callback is invoked in an invalid request for verification is made,
-            // for instance if the the phone number format is not valid.
-            if (e is FirebaseAuthInvalidCredentialsException) {
-                // Invalid request
-                requireActivity().msg("Invalid request")
-            } else if (e is FirebaseTooManyRequestsException) {
-                // The SMS quota for the project has been exceeded
-                requireActivity().msg("Too many request")
-            }
-            // Show a message and update the UI
-        }
-
-        override fun onCodeSent(
-            verificationId: String,
-            token: PhoneAuthProvider.ForceResendingToken
-        ) {
-            // The SMS verification code has been sent to the provided phone number, we
-            // now need to ask the user to enter the code and then construct a credential
-            // by combining the code with a verification ID.
-            // Save verification ID and resending token so we can use them later
-            otpArgument = verificationId
-            resendToken = token
-        }
-    }
     private fun validateInput(otp : String) {
-        //TODO : VALIDATE IF OTP IS CORRECT
-        otpArgument = arguments?.getString("OTP").toString()
-        val credential : PhoneAuthCredential = PhoneAuthProvider.getCredential(
-            otpArgument, otp
-        )
+        argOtp = arguments?.getString("otp").toString()
+        val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(argOtp, otp)
         signInWithPhoneAuthCredential(credential)
     }
+
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(requireActivity()) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    requireActivity().msg("Success")
-                } else {
-                    // Sign in failed, display a message and update the UI
-                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                        // The verification code entered was invalid
-                        requireActivity().msg("Invalid")
-                    }
-                    // Update UI
-                }
+        auth.signInWithCredential(credential).apply {
+            addOnSuccessListener {
+                requireActivity().msg("Success")
             }
+            addOnFailureListener {
+                requireActivity().msg("Invalid")
+            }
+        }
     }
-    private fun sendArguments() {
-        //TODO : GET EMAIL AND PHONE NUMBER FROM FrgForgotPassword.kt
-    }
+//    private fun sendArguments() {
+//        //TODO : GET EMAIL AND PHONE NUMBER FROM FrgForgotPassword.kt
+//    }
     //INPUT VALIDATOR
     private fun textWatcher(textField : EditText) {
         with(bind) {
